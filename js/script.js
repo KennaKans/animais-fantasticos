40df53a7d8d91c81848da237ce81f34d5c4cb89e
Alexia Caroline Fiquer Pereira <Alexia@cpdservidor.com>
2023-08-24 12:08:59 -0300

    tooltip refatorado

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
-import initTooltip from './modules/tooltip.js';
+import Tooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/schedule.js';
@@ -29,8 +29,10 @@ const modal = new Modal(
 );
 modal.init();
 
+const tooltip = new Tooltip('[data-tooltip]');
+tooltip.init();
+
 initAnimacaoScroll();
-initTooltip();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
